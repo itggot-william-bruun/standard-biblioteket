3aa287ad7b3eed6a854898271ae660daaaede03e
itggot-william-bruun <38528636+itggot-william-bruun@users.noreply.github.com>
2018-05-03 14:08:33 +0200

    created and documented strip method

---
+++ lib/strip.rb
@@ -0,0 +1,12 @@
+#Public: Strips a string of unecessary symbols
+#
+# string - The string to be cleaned
+#
+#Examples
+#
+#   strip("\nHello World      \n")
+#   # => "Hello World"
+#Returns the cleaned string
+def strip(string)
+    return string.strip
+end
